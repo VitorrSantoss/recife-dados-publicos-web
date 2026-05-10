3c67e6ece30a2570cdd3f9ec0bf2efa81ba2de59
Vitor Santos <vitorvitorsantos2015@hotmail.com>
2026-05-10 17:46:57 -0300

    Inicial: Projeto Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Saúde Dashboard</h1>
+      <p>Seu projeto está pronto para desenvolvimento!</p>
+    </div>
+  );
+}
+
+export default App;
